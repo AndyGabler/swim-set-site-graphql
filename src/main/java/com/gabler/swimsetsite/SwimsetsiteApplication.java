3dee161502fb18cbcdf6c7f306cbb7d05a2f77ae
AndronikusGameTech <andronikus1335@gmail.com>
2025-01-08 21:14:27 -0500

    initial commit

---
+++ src/main/java/com/gabler/swimsetsite/SwimsetsiteApplication.java
@@ -0,0 +1,12 @@
+package com.gabler.swimsetsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimsetsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimsetsiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gabler/swimsetsite/SwimsetsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabler.swimsetsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimsetsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
